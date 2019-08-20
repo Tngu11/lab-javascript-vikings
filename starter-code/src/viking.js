6f519d2a0a5cb18cf2ceb89b25a925851acad32a
Tngu11 <lua.shea33@gmail.com>
2019-08-20 20:33:54 +0200

    wip

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,42 @@
 // Soldier
-class Soldier {}
+
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+
+  receiveDamage() {
+    this.health -= damage;
+  if (this.health > 0) {
+    return this.name + "has received" + receiveDamage() + "points of damage"
+  }
+    else return this.name + "has died in act of combat"
+  }
+}
+
+//  const Viking = new Viking ("Harald", 300, 150);
+
+// new Viking.receiveDamage(50)
+// new Viking.receiveDamage(75)
 
 // Saxon
 class Saxon {}
